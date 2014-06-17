28df6f0e1bc9116bf73ed3a66a400b3bda2b33d0
Tracy Pesin <tracy@songza.com>
2014-06-17 10:18:40 -0400

    Initial Commit

---
+++ CollectionTest/STAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STAppDelegate.h
+//  CollectionTest
+//
+//  Created by Tracy Pesin on 6/17/14.
+//  Copyright (c) 2014 Songza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CollectionTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CollectionTest
+//
+//  Created by Tracy Pesin on 6/17/14.
+//  Copyright (c) 2014 Songza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "STAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([STAppDelegate class]));
+    }
+}
